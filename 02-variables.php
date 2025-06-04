12407522ff53c6ba7c20701d787ab879f3d9ef60
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2025-06-04 13:47:14 -0600

    Variables en PHP (y constantes)

--- 02-variables.php
+++ 02-variables.php
@@ -1,6 +1,27 @@
 <?php include 'includes/header.php';
 
 
-echo"<p>Adios mundo</p>";
+$nombre = "Moi";
+$usuarios99 = 99;
+
+$_nombre = "Moises";
+
+$nombre_ = "Pepe";
+
+$nombre = "Carmela";
+
+echo $nombre_;
+
+var_dump($nombre);
+
+define("constante","Este es el valor de la constante");
+
+echo constante;  // Sin signo de dolar
+
+const constante2 = "Hola 2";
+echo constante2;
+
+$nombreCliente = "Pedro";
+$nombre_cliente = "Pedro";
 
 include 'includes/footer.php';
