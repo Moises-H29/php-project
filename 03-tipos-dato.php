a2e993be927804271f8a22adfb0a2b055634b19d
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2025-06-05 14:21:57 -0600

    Tipos de datos

--- 03-tipos-dato.php
+++ 03-tipos-dato.php
@@ -1,5 +1,25 @@
 <?php include 'includes/header.php';
 
+// Boolean
+$logueado = false;
+
+var_dump($logueado);
+
+// Enteros
+$numero = 300;
+var_dump($numero);
+
+// Floats
+$flotante = 3.14;
+var_dump($flotante);
+
+// Strings
+$nombre = "Moises";
+var_dump($nombre);
+
+// Arreglos
+$array = [1,2,3,"Jola"];
+var_dump($array);
 
 
 
