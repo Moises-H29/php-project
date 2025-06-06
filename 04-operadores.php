e6cf78bad5f20b7acb99127bbc1f77c5f2dc4d15
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2025-06-06 17:30:51 -0600

    Operadores en PHP

--- 04-operadores.php
+++ 04-operadores.php
@@ -1,6 +1,21 @@
 <?php include 'includes/header.php';
 
+$numero1 = 10;
+$numero2 = 20;
 
+// Suma
+echo $numero1 + $numero2,"<br>";
 
+// Resta
+echo $numero1 - $numero2,"<br>";
+
+// Multiplicacion
+echo $numero1 * $numero2,"<br>";
+
+// Division
+echo $numero1 / $numero2;
+
+// Multiplicar cierta cantidad de veces
+echo 2 ** 3;
 
 include 'includes/footer.php';
