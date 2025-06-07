1a8885e6c4123aafc767c20decf5d185664b30bb
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2025-06-06 19:06:49 -0600

    Incremento y decremento en PHP

--- 06-inc-dec.php
+++ 06-inc-dec.php
@@ -1,6 +1,20 @@
 <?php include 'includes/header.php';
 
+$numero1 = 30;
+
+//$numero1++;
+
+echo $numero1++;
+
+echo ++$numero1;
+
+$numero1+=5;
+
+echo "<br>";
+echo $numero1+=33;
 
 
+$numero2 = 40;
+echo $numero2--;
 
 include 'includes/footer.php';
