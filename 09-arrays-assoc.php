a50540a0fe496f7c260d16475a6f6e5c0904cd3b
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2025-06-13 16:50:20 -0600

    Associative Arrays PHP

--- 09-arrays-assoc.php
+++ 09-arrays-assoc.php
@@ -1,6 +1,28 @@
 <?php include 'includes/header.php';
 
+// Se utilizan como los objetos en JavaScript
 
+$cliente = array(
+    'nombre' => 'Moises',
+    'saldo' => 200,
+    'informacion' => [
+        'tipo' => 'premium',
+        'disponible' => 100
+    ]
+);
+
+echo "<pre>";
+var_dump($cliente['informacion']);
+echo "</pre>";
+
+// echo $cliente['nombre'];
+// echo $cliente['informacion']['disponible'];
+
+$cliente['codigo'] = 5484984151616;
+
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
 
 
 include 'includes/footer.php';
