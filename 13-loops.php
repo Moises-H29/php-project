a55714643bc5bff80e50d47d24bb517249e91d12
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2025-06-23 19:57:45 -0600

    Bucles en PHP

--- 13-loops.php
+++ 13-loops.php
@@ -1,5 +1,48 @@
 <?php include 'includes/header.php';
 
+// While
+
+$i = 100;  // Inicializador
+
+while($i < 10) {
+    echo $i . "<br>";
+    $i++;  // Incremento
+}
+
+echo "<br>";
+
+// Do while
+
+$i = 100;
+
+do {
+    echo $i ."<br>";
+    $i++;
+} while($i < 10);
+
+echo "<br>";
+
+// For
+
+for($i = 0; $i < 10; $i++){
+    echo $i."<br>";
+}
+
+echo "<br><br>";
+// FizzBuzz
+
+for($i=1; $i<=1000; $i++){
+    if($i % 3 === 0 && $i % 5 === 0){
+        echo $i." - FIZZ BUZZ <br>";
+    }else if($i % 3 === 0){
+        echo $i." - Fizz <br>";
+    }else if($i % 5 === 0){
+        echo $i." - Buzz <br>";
+    }else{
+        echo $i."<br>";
+    }
+}
+
 
 
 
