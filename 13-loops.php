81fd45aebc20185a2038cf19c0c1a82315d5ead3
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2025-06-25 00:38:54 -0600

    Loops en PHP (final)

--- 13-loops.php
+++ 13-loops.php
@@ -2,12 +2,17 @@
 
 // While
 
-$i = 100;  // Inicializador
+$i = 0;  // Inicializador
 
-while($i < 10) {
+while($i < 4):
     echo $i . "<br>";
+    if($i===0):
+        echo "Hola <br>";
+    endif;
+
     $i++;  // Incremento
-}
+
+endwhile;
 
 echo "<br>";
 
@@ -22,28 +27,51 @@ do {
 
 echo "<br>";
 
-// For
+// For loop
 
 for($i = 0; $i < 10; $i++){
     echo $i."<br>";
 }
 
 echo "<br><br>";
+
 // FizzBuzz
 
-for($i=1; $i<=1000; $i++){
-    if($i % 3 === 0 && $i % 5 === 0){
-        echo $i." - FIZZ BUZZ <br>";
-    }else if($i % 3 === 0){
-        echo $i." - Fizz <br>";
-    }else if($i % 5 === 0){
-        echo $i." - Buzz <br>";
-    }else{
-        echo $i."<br>";
-    }
+// for($i=1; $i<=1000; $i++){
+//     if($i % 3 === 0 && $i % 5 === 0){
+//         echo $i." - FIZZ BUZZ <br>";
+//     }else if($i % 3 === 0){
+//         echo $i." - Fizz <br>";
+//     }else if($i % 5 === 0){
+//         echo $i." - Buzz <br>";
+//     }else{
+//         echo $i."<br>";
+//     }
+// }
+
+
+// For Each (recorre arreglos)
+$clientes = ['Pedro', 'Juan', 'Karen'];
+
+foreach( $clientes as $cliente ):
+    echo $cliente . "<br>";
+endforeach;
+
+for($i=0; $i < count($clientes); $i++){
+    echo $clientes[$i]."<br>";
 }
 
+$cliente = array(
+    "nombre" => "Juan",
+    "saldo" => 200,
+    "tipo" => "Premium"
+);
 
-
+foreach( $cliente as $key => $valor ):
+    echo $key." - ".$valor."<br>";
+    if($valor === 200){
+        echo "Es 200 <br>";
+    }
+endforeach;
 
 include 'includes/footer.php';
