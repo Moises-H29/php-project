a17847432fd806a6705fde17c2f837efdeba9691
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2025-07-22 15:14:50 -0600

    Union de retornos de funciones

--- 15-funciones.php
+++ 15-funciones.php
@@ -2,18 +2,18 @@
 declare( strict_types = 1 );  // Obtener en tiempo real un error de tipado
 include 'includes/header.php';
 
-function sumar(int $numero1 = 0, int $numero2 = 0) {
+function sumar(int $numero1 = 0, bool $numero2) {  // Podemos definir el tipo de dato que espera la función
     echo $numero1 + $numero2;
 }
 
-// sumar(10, 20);
-// echo "<br>";
-// sumar(3, -15);
-// echo "<br>";
-// sumar(19, 2);
-// echo "<br>";
-// sumar(100000, 20);
-// echo "<br>";
+sumar(10, 20);
+echo "<br>";
+sumar(3, -15);
+echo "<br>";
+sumar(19, 2);
+echo "<br>";
+sumar(100000, 20);
+echo "<br>";
 
 sumar(10, 20);
 
--- 16-types.php
+++ 16-types.php
@@ -1,6 +1,16 @@
-<?php include 'includes/header.php';
+<?php declare(strict_types=1);
+include 'includes/header.php';
 
+function usuarioAutenticado(bool $autenticado) : string|int {  // Unión del tipo de retorno de valor
+    if($autenticado){
+        return "El usuario esta autenticado";
+    }else{
+        return 6;
+    }
+}
 
+$usuario = usuarioAutenticado(false);
+echo $usuario;
 
 
 
