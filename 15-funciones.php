0b2ff2edd973633005bfef937eb56dd9cb8505c6
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2025-07-22 15:23:51 -0600

    Parametros nombrados en PHP 8

--- 15-funciones.php
+++ 15-funciones.php
@@ -2,20 +2,13 @@
 declare( strict_types = 1 );  // Obtener en tiempo real un error de tipado
 include 'includes/header.php';
 
-function sumar(int $numero1 = 0, bool $numero2) {  // Podemos definir el tipo de dato que espera la función
+function sumar(int $numero1 = 0, int $numero2 = 0) {  // Podemos definir el tipo de dato que espera la función
+    echo $numero1;
+    echo "<br>";
     echo $numero1 + $numero2;
 }
 
-sumar(10, 20);
-echo "<br>";
-sumar(3, -15);
-echo "<br>";
-sumar(19, 2);
-echo "<br>";
-sumar(100000, 20);
-echo "<br>";
-
-sumar(10, 20);
+sumar(numero2:10, numero1: 20);  // Parámetros nombrados (Named parameters)
 
 
 include 'includes/footer.php';
