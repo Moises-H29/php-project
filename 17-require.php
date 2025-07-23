ab26ac6c646e7a045081afe84f9d6c9d14fc7339
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2025-07-22 21:36:08 -0600

    Rquire en PHP

--- 17-require.php
+++ 17-require.php
@@ -1,7 +1,13 @@
 <?php include 'includes/header.php';
 
 
+require 'funciones.php';
 
+iniciarApp();
+
+// include - Sigue ejecutando aunque no encuentre el archivo
+// require - Lanza un error si no encuentra el archivo
+// require_once - Si el archivo ya se incluyó, se ignora
 
 
 
